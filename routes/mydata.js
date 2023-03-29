ed3aab5841b884ff451eea1d50841bcb3ba11ba7
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-03-29 18:41:35 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Supriya Bodapati' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
