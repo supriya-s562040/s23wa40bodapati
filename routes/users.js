38adc6d23a09e4c2bc8a7ab86bff73a64ee90983
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-03-29 18:56:18 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ let more=0;
 router.get('/', function(req, res, next) {
   more+=1;
   sum+=more;
-  res.send('respond with a resource');
+  res.send('Sum is: '+sum);
 });
 
 module.exports = router;
