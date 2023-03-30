ec66310bacbdd8e991cde8add4044e4214c163d0
Supriya Bodapati <supriyabodapati94@gmail.com>
2023-03-29 19:49:24 -0500

    bonus

---
+++ routes/computation.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+let x=Math.floor(Math.random());
+let y
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    if(req.query.x!=undefined){
+        x=req.query.x;
+
+    }
+    let r=Math.fround(x);
+    let t=Math.random(x);
+    let n=Math.round(x);
+    
+  res.render('computation', { x:x,log:r, cos:t, floor:n});
+});
+
+module.exports = router;
